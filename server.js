92f8313d9b9b0a205efeb700e2f2bd7c46dee876
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-20 12:29:31 -0500

    Middleware firing

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require("helmet");
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+//Middleware
+function bouncer(req, res, next) {
+  res.status(404).json("These are not the droids you are looking for");
+}
+
+function teamer(req, res, next) {
+  req.team = "Web 17";
+  next();
+}
+
+server.use(teamer);
 server.use(express.json());
+server.use(helmet());
 
-server.use('/api/hubs', hubsRouter);
+//Routing
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res, next) => {
+//Route handlers ARE middleware
+server.get("/", (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
 
